60e5d2615ba9e370a920e3645c9fc7920db39a3d
Tara Bahadur Thapa <tarabahadur.thapa@gmail.com>
2024-05-20 16:30:57 +0545

    Update WebClientConfig.java

--- microservices-new/order-service/src/main/java/com/microservices/orderservice/config/WebClientConfig.java
+++ microservices-new/order-service/src/main/java/com/microservices/orderservice/config/WebClientConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
-
+    
     @Bean
     public WebClient webClient(){
         return WebClient.builder().build();
